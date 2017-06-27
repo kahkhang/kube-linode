4e2338153d0cfc2c3a71c15866ad14f4e4103ca0
Andrew Low <andrew.low@ninjavan.co>
2017-06-27 18:51:18 +0800

    use no cache in install script

--- install.sh
+++ install.sh
@@ -7,17 +7,17 @@ echo -ne '                          (0%)\r'
 [ -e ~/.kube-linode/utilities.sh ] && rm ~/.kube-linode/utilities.sh
 
 echo -ne '#####                     (33%)\r'
-curl -s https://raw.githubusercontent.com/kahkhang/kube-linode/master/install-coreos.sh > ~/.kube-linode/install-coreos.sh
+curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/kahkhang/kube-linode/master/install-coreos.sh > ~/.kube-linode/install-coreos.sh
 echo -ne '#############             (66%)\r'
-curl -s https://raw.githubusercontent.com/kahkhang/kube-linode/master/kube-linode.sh > ~/.kube-linode/kube-linode.sh
+curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/kahkhang/kube-linode/master/kube-linode.sh > ~/.kube-linode/kube-linode.sh
 
-curl -s https://raw.githubusercontent.com/kahkhang/kube-linode/master/inquirer_common.sh > ~/.kube-linode/inquirer_common.sh
-curl -s https://raw.githubusercontent.com/kahkhang/kube-linode/master/checkbox_input.sh > ~/.kube-linode/checkbox_input.sh
-curl -s https://raw.githubusercontent.com/kahkhang/kube-linode/master/text_input.sh > ~/.kube-linode/text_input.sh
-curl -s https://raw.githubusercontent.com/kahkhang/kube-linode/master/list_input.sh > ~/.kube-linode/list_input.sh
+curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/kahkhang/kube-linode/master/inquirer_common.sh > ~/.kube-linode/inquirer_common.sh
+curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/kahkhang/kube-linode/master/checkbox_input.sh > ~/.kube-linode/checkbox_input.sh
+curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/kahkhang/kube-linode/master/text_input.sh > ~/.kube-linode/text_input.sh
+curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/kahkhang/kube-linode/master/list_input.sh > ~/.kube-linode/list_input.sh
 
 echo -ne '####################      (90%)\r'
-curl -s https://raw.githubusercontent.com/kahkhang/kube-linode/master/utilities.sh > ~/.kube-linode/utilities.sh
+curl -s -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/kahkhang/kube-linode/master/utilities.sh > ~/.kube-linode/utilities.sh
 
 chmod +x ~/.kube-linode/install-coreos.sh
 chmod +x ~/.kube-linode/kube-linode.sh
